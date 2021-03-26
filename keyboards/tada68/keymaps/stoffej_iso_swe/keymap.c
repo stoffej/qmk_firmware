417c484721fa32789a9843ddeda734eb23100eda
stoffej <kristoffer.c.johansson@gmail.com>
2021-03-26 12:36:05 +0000

    end start

--- keyboards/tada68/keymaps/stoffej_iso_swe/keymap.c
+++ keyboards/tada68/keymaps/stoffej_iso_swe/keymap.c
@@ -15,10 +15,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  +|  '|Backspa |  ´|
 <<<<<<< HEAD
+<<<<<<< HEAD
    * |----------------------------------------------------------------|l
 =======
    * |----------------------------------------------------------------|
 >>>>>>> db2c6920d6 (added my tada68)
+=======
+   * |----------------------------------------------------------------|l
+>>>>>>> e6ac602f69 (end start)
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  Å|  ¨| Ent-|Del |
    * |-------------------------------------------------------| er|----|
    * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  Ø|  Æ| @ |   |PgUp|
@@ -32,10 +36,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_1, KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,  \
     KC_TAB,     KC_Q, KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_DEL,  \
 <<<<<<< HEAD
+<<<<<<< HEAD
     LT(_FL_, KC_BSPC),  KC_A , KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
 =======
     MO(_FL_),  KC_A , KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
 >>>>>>> db2c6920d6 (added my tada68)
+=======
+    LT(_FL_, KC_BSPC),  KC_A , KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
+>>>>>>> e6ac602f69 (end start)
     KC_LSPO, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN, \
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_FL_), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
@@ -57,10 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,     KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12,    RESET,   KC_PSCR, \
     _______, _______,   KC_UP,  M_NAME , _______, _______, _______, _______, _______,  BL_DEC,  BL_INC,  BL_TOGG, _______,              KC_INS,  \
 <<<<<<< HEAD
+<<<<<<< HEAD
     _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______,  _______, _______,  _______,   KC_HOME, \
 =======
     _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, _______, _______,  _______, _______,  _______,   KC_HOME, \
 >>>>>>> db2c6920d6 (added my tada68)
+=======
+    _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______,  _______, _______,  _______,   KC_HOME, \
+>>>>>>> e6ac602f69 (end start)
     _______, _______, _______, _______,  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLD, KC_MUTE,  KC_VOLU, _______, M_CD_DOT,    KC_END,  \
     _______, _______, _______,                   _______,                             _______, _______,   KC_APP, _______,  _______,    _______
   ),
