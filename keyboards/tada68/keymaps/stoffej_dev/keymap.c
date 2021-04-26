32dfa3b1a2ea787685517ddfb67d80942bc10548
stoffej <kristoffer.c.johansson@gmail.com>
2021-04-26 20:39:00 +0000

    wide and angle mod
    extended layer update

--- keyboards/tada68/keymaps/stoffej_dev/keymap.c
+++ keyboards/tada68/keymaps/stoffej_dev/keymap.c
@@ -134,6 +134,7 @@ const uint32_t PROGMEM unicode_map[] = {
 #define FN_BSPC LT(_FUNCTION, KC_BSPC)
 #define FN_SCLN LT(_FUNCTION, KC_SCLN)
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define FN_KC_N LT(_FUNCTION, KC_N)
 #define FN_KC_T LT(_FUNCTION, KC_T)
 #define SW_KC_E LT(_SWE, KC_E)
@@ -145,6 +146,10 @@ const uint32_t PROGMEM unicode_map[] = {
 
 #define SHFT_KC_A LSFT_T(KC_A)
 #define SHFT_KC_O RSFT_T(KC_O)
+=======
+#define FN_KC_N LT(_FUNCTION, KC_N)
+#define FN_KC_T LT(_FUNCTION, KC_T)
+>>>>>>> 03ab466045 (wide and angle mod)
 
 #define CTLZ LCTL(KC_Z)
 #define CTLX LCTL(KC_X)
@@ -311,8 +316,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
-   */
+   *
+  * [_COLEMAK] = LAYOUT_iso(
+  *   KC_ESC , KC_1   , KC_2   ,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,  \
+  *   EM_TAB , KC_Q   , KC_W   ,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    FN_SCLN,    KC_LBRC, KC_RBRC,          KC_DEL,  \
+  *   FN_BSPC, KC_A   , KC_R   ,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
+  *   KC_LSPO, KC_NUBS, KC_Z   ,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN, \
+  *   KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                       KC_RALT, MO(_FUNCTION), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+  * ),
+
+*/
   [_COLEMAK] = LAYOUT_iso(
+<<<<<<< HEAD
 <<<<<<< HEAD
     KC_ESC , TD(TD_1)   , TD(TD_2)   ,    TD(TD_3),    TD(TD_4),    TD(TD_5),    TD(TD_6),    TD(TD_EQL),  TD(TD_7),    TD(TD_8),    TD(TD_9),    TD(TD_0),    TD(TD_DASH), KC_BSPC,     TD(TD_GRAV),  \
     EM_TAB , KC_Q   , KC_W   ,    KC_F,    KC_P,    KC_B,    KC_SE_LBRC, KC_J,    KC_L,    KC_U,    KC_Y,    SCLN, SLSH,              KC_DEL,  \
@@ -329,6 +344,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     FN_BSPC, KC_A   , KC_R   ,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
     KC_LSPO, KC_NUBS, KC_Z   ,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN, \
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                       KC_RALT, MO(_FUNCTION), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+=======
+    KC_ESC , KC_1   , KC_2   ,    KC_3,    KC_4,    KC_5,    KC_6,    KC_EQL,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC,     KC_GRV,  \
+    EM_TAB , KC_Q   , KC_W   ,    KC_F,    KC_P,    KC_B,    KC_LBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_SLSH,              KC_DEL,  \
+    FN_BSPC, KC_A   , KC_R   ,    KC_S,    FN_KC_T,    KC_G,    KC_RBRC, KC_M,    FN_KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,      KC_PGUP, \
+    KC_LSPO, KC_Z   , KC_X   ,    KC_C,    KC_D,    KC_V,    KC_BSLS, KC_HASH, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_RSPC, KC_UP,       KC_PGDN, \
+    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                       KC_RALT, MO(_FUNCTION), KC_RCTL, KC_LEFT, KC_DOWN,        KC_RGHT
+>>>>>>> 03ab466045 (wide and angle mod)
   ),
 
 
@@ -359,7 +381,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_EMOJI] = LAYOUT_iso(
     _______, _______, _______, _______, _______, _______, _______,  _______,   _______, _______, _______, _______, _______,  _______, _______,  \
     _______, _______, _______, _______, _______, X(USMIL),  X(UKB), X(UCOFFEE), X(UVOLT), X(UBEER), X(UTHMUP),XP(SE_AA_L, SE_AA_H) , _______,           _______,  \
+<<<<<<< HEAD
     _______, _______, _______, _______, _______, X(UORHEART), X(UBIC), X(UNERD),   X(UBUG), X(UPARTY), XP(SE_OE_L, SE_OE_H), XP(SE_AE_L, SE_AE_H) , _______,           _______,  _______, \
+=======
+    _______, _______, _______, OS_WIN, OS_LIN, X(UORHEART), X(UBIC), X(UNERD),   X(UBUG), X(UPARTY), XP(SE_OE_L, SE_OE_H), XP(SE_AE_L, SE_AE_H) , _______,           _______,  _______, \
+>>>>>>> 03ab466045 (wide and angle mod)
     _______, BL_TOGG, BL_DEC, _______, _______, _______, X(UWHALE), X(UBOMB), X(UFACE_ROLLING_EYES), X(THNK), _______, _______, _______, _______,  _______, \
     _______, _______, _______,                   _______,                             _______,_______, _______, _______, _______, _______
   ),
@@ -453,9 +479,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 >>>>>>> 408cd650dc (add mouse key)
 =======
     _______, KC_F1  ,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_F7  , KC_F8   , KC_F9 ,  KC_F10,   KC_F11,  KC_F12,    RESET,   KC_PSCR, \
-    _______, LAY_COL, LAY_QWE,  M_NAME, _______, _______, _______, KC_HOME, KC_END  , BL_INC,  _______, XP(SE_AA_L, SE_AA_H) , _______,              KC_INS,  \
-    _______, OS_WIN , OS_LIN , VM2WIN , WIN2VM , KC_DEL , KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, XP(SE_OE_L, SE_OE_H) ,  XP(SE_AE_L, SE_AE_H) , _______, _______,   KC_HOME, \
-    _______, BL_TOGG, BL_DEC , _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLD, KC_MUTE,  KC_VOLU, _______, M_CD_DOT,    KC_END,  \
+    _______, LAY_COL, LAY_QWE,  M_NAME, _______, _______, KC_PGUP, KC_HOME, KC_UP   , KC_END,  KC_DEL,  KC_ESC ,KC_INS ,           _______  ,  \
+    _______,  VM2WIN,  WIN2VM, KC_LSHIFT, KC_LCTRL , KC_DEL ,_______ ,KC_PGDN ,  KC_LEFT ,KC_DOWN, KC_RIGHT, KC_BSPC ,_______  , _______,   KC_HOME, \
+    _______, _______,CTLZ ,CTLX , CTLC, CTLV,KC_MPRV, KC_MPLY, KC_MNXT,  KC_VOLD, KC_MUTE,  KC_VOLU, _______, M_CD_DOT,    BL_INC,  \
     _______, _______, _______,                   KC_ENT ,                             _______, _______,   KC_APP, _______,  _______,    _______
 >>>>>>> 939dee165a (delay for OS switch)
   ),
@@ -490,7 +516,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed) {
             SEND_STRING( SS_DOWN(X_LCTL) SS_DOWN(X_LALT) SS_UP(X_LCTL) SS_UP(X_LALT) SS_DOWN(X_LCTL) SS_DOWN(X_LGUI) SS_TAP(X_LEFT) SS_UP(X_LCTL) SS_UP(X_LGUI));
             backlight_level(3);
-            _delay_ms(2000);
+            _delay_ms(1000);
             set_unicode_input_mode(UC_WINC);
             backlight_level(0);
         }
