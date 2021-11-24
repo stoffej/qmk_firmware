443f84c168a454ecd56db0bdf7d6ca9b75b01f85
stoffej <kristoffer.c.johansson@gmail.com>
2021-11-24 21:43:15 +0000

    added new board

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/stoffej/config.h
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/stoffej/config.h
@@ -24,8 +24,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MASTER_RIGHT
 
 //#define EE_HANDS
+<<<<<<< HEAD
 <<<<<<< HEAD:keyboards/handwired/dactyl_manuform/6x6/keymaps/stoffej/config.h
 #define SPLIT_HAND_PIN D1
 =======
 #define SPLIT_HAND_PIN D1
 >>>>>>> 5b8286df46 (before setting default to swedish in OS):keyboards/handwired/dactyl_manuform/6x6/keymaps/default/config.h
+=======
+#define SPLIT_HAND_PIN D1
+>>>>>>> 8a13e0efee (added new board)
